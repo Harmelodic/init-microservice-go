10d03a1432ac8737fc33340bf8c1ba6e4579938e
Harmelodic <matt@harmelodic.com>
2025-06-23 13:40:48 +0200

    Some initial bits from gin

--- src/main.go
+++ src/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func main() {
-	// Some basic code in place whilst setting up CI
-	// TODO: Replace with a proper application
-	fmt.Println("Hello, World!")
+	// TODO: Replace with real log
+	fmt.Println("Starting service...")
+
+	router := gin.Default()
+
+	// TODO: Move this out to a management package for health checks, observability scraping and mgmt endpoints.
+	router.GET("/management/health", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"status": "UP",
+		})
+	})
+
+	err := router.Run()
+	if err != nil {
+		// TODO: Replace with real log
+		fmt.Println("Error occurred when starting Gin app. Exiting")
+		os.Exit(1)
+	}
 }
