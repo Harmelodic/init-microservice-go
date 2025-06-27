c63b9ebe7b17edfbd2edb9b2aa752dd8029d6525
Harmelodic <matt@harmelodic.com>
2025-06-27 18:50:28 +0200

    Adding gin factory testing

---
+++ internal/commons/gin_config_test.go
@@ -0,0 +1,32 @@
+package commons
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// Testing
+
+func TestNewGinEngine_Recovery(t *testing.T) {
+	// Given
+	testEngine := NewGinEngine(slog.New(slog.DiscardHandler))
+	testEngine.GET("/endpoint", func(context *gin.Context) {
+		panic(1)
+	})
+
+	// When
+	responseRecorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/endpoint", nil)
+	testEngine.ServeHTTP(responseRecorder, req)
+
+	// Then
+	assert.Equal(t, 500, responseRecorder.Code)
+	assert.Equal(t, "", responseRecorder.Body.String())
+}
+
+// TODO: Test logs with Trace ID
+// TODO: Test Release Mode
