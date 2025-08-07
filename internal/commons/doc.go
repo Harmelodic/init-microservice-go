9006ad2b9fbbefdfd6c4fbda5f47a99bf62671d6
Harmelodic <matt@harmelodic.com>
2025-08-07 13:22:41 +0200

    Reason for commons

--- internal/commons/doc.go
+++ internal/commons/doc.go
@@ -1,2 +1,5 @@
 // Package commons handles common non-domain features
+//
+// Some of these could be in `cmd/app` main package, but I suspect these will be reusable across apps, and so I would
+// prefer to make these easily copy-pasteable, or be easy to create an actually public `commons` library.
 package commons
